a7ed54a864b53367c0779a82839646cf01606243
Aviral Gupta <122053473+stark0906@users.noreply.github.com>
2025-03-25 11:56:26 +0530

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '68783c0d-2a90-4390-809a-4cea85676aa7';
+export const MODAL_CLOSE_SEC = 2.5;
